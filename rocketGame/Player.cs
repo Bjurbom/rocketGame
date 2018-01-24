8b7edfcc5105b16db5ccc9ee9311a7ae38b49e5b
tor bjurbom <bjurbom1@gmail.com>
2018-01-24 21:09:57 +0100

    abstract sak
    Idk what im doing

---
+++ rocketGame/Player.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace rocketGame
+{
+    class Player:GameObject
+    {
+
+        
+        public Player(Texture2D sprite, Vector2 posistion):base(sprite,posistion)
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw()
+        }
+    }
+}
